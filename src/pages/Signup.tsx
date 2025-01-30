993f73005fabb84539f03fe590d09996b584b3c0
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2025-01-29 20:55:41 -0700

    Add signup page

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,109 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { NavLink } from 'react-router';
+import { z } from 'zod';
+import { signUp } from '../api/auth';
+
+const schema = z.object({
+    email: z
+        .string()
+        .email('Invalid email address')
+        .nonempty('Email is required'),
+    password: z
+        .string()
+        .min(8, 'Password must be at least 8 characters')
+        .nonempty('Password is required'),
+    username: z.string().min(8).nonempty(),
+});
+
+type FormFields = z.infer<typeof schema>;
+
+const SignupPage = () => {
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors, isSubmitting },
+    } = useForm<FormFields>({ resolver: zodResolver(schema) });
+
+    const onSubmit: SubmitHandler<FormFields> = async (data) => {
+        try {
+            const token = await signUp(
+                data.email,
+                data.password,
+                data.username
+            );
+            localStorage.setItem('authToken', token);
+            window.location.reload();
+        } catch (error) {
+            setError('root', {
+                message:
+                    error instanceof Error
+                        ? error.message
+                        : 'An unknown error occurred.',
+            });
+        }
+    };
+
+    return (
+        <div className="flex flex-col w-screen h-screen items-center justify-center">
+            <h2 className="text-6xl font-semibold mb-12">Sign Up</h2>
+            <form
+                className="flex flex-col w-full max-w-[600px] p-4 min-h-0"
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <label className="text-left text-3xl mb-2">Email</label>
+                <input
+                    {...register('email')}
+                    className="shadow border rounded py-1 pl-2 text-3xl"
+                    type="email"
+                    placeholder="Enter your email"
+                />
+                {errors.email && (
+                    <div className="text-red-500">{errors.email.message}</div>
+                )}
+
+                <label className="text-left text-3xl mt-2 mb-2">Password</label>
+                <input
+                    {...register('password')}
+                    className="shadow border rounded py-1 pl-2 text-3xl"
+                    type="password"
+                    placeholder="Enter your password"
+                />
+
+                <label className="text-left text-3xl mt-2 mb-2">Username</label>
+                <input
+                    {...register('username')}
+                    className="shadow border rounded py-1 pl-2 text-3xl"
+                    type="text"
+                    placeholder="Enter your username"
+                />
+
+                {errors.password && (
+                    <div className="text-red-500">
+                        {errors.password.message}
+                    </div>
+                )}
+
+                <button
+                    className="text-3xl font-bold rounded shadow mt-5 p-2 bg-gray-200 hover:bg-gray-300"
+                    disabled={isSubmitting}
+                    type="submit"
+                >
+                    Sign up
+                </button>
+
+                {/*errors.root && (
+                <div className="text-red-500">{errors.root.message}</div>
+            )*/}
+                <div className="items-start mt-3">
+                    <NavLink to="/" className="text-2xl">
+                        Log In
+                    </NavLink>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SignupPage;

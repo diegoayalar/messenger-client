c64252e50052e7b8368bd9716425705ecca4a01a
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2025-01-29 20:55:02 -0700

    Add api functions for login and signup

---
+++ src/api/auth.ts
@@ -0,0 +1,38 @@
+export const logIn = async (
+    email: string,
+    password: string
+): Promise<string> => {
+    const response = await fetch('http://localhost:7279/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data.Token;
+};
+
+export const signUp = async (
+    email: string,
+    password: string,
+    username: string
+): Promise<string> => {
+    const response = await fetch('http://localhost:7279/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password, username }),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data.Token;
+}

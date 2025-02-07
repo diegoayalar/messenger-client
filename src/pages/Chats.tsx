418e1bdff3bc1847fa7e3406f20305d7f1ad7996
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2025-02-06 19:44:20 -0700

    Add sidebar to main page

--- src/pages/Chats.tsx
+++ src/pages/Chats.tsx
@@ -1,23 +1,13 @@
-const Chats = () => {
-    const handleLogOut = () => {
-        localStorage.removeItem('authToken');
-        window.location.reload();
-    };
+import Sidebar from '../components/Sidebar';
 
+const Chats = () => {
     return (
-        <div className="flex flex-col min-h-screen items-center justify-center bg-gray-100">
-            <h1 className="text-4xl font-bold mb-5">
-                Welcome to chats!
-            </h1>
-            <div className="justify-end">
-                <button
-                    onClick={handleLogOut}
-                    className="text-3xl font-normal rounded shadow p-2 bg-gray-200 hover:bg-gray-300"
-                >
-                    Log out
-                </button>
+        <>
+        <Sidebar/>
+            <div className="flex flex-col min-h-screen items-center justify-center bg-gray-100">
+                <h1 className="text-4xl font-bold mb-5">Welcome to chats!</h1>
             </div>
-        </div>
+        </>
     );
 };
 

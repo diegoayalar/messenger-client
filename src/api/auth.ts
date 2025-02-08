bd7fcf3d1c6b05e53ddf80b47d657c2f42629556
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2025-02-07 21:33:15 -0700

    Add credentials in requests

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,31 +1,24 @@
-export const logIn = async (
-    email: string,
-    password: string
-): Promise<string> => {
+export const logIn = async (email: string, password: string): Promise<void> => {
     const response = await fetch('http://localhost:7279/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
+        credentials: 'include',
     });
 
-    if (!response.ok) {
-        throw new Error(await response.text());
-    }
-
-    const data = await response.json();
-
-    return data.Token;
+    if (!response.ok) throw new Error(await response.text());
 };
 
 export const signUp = async (
     email: string,
     password: string,
     username: string
-): Promise<string> => {
+): Promise<void> => {
     const response = await fetch('http://localhost:7279/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password, username }),
+        credentials: 'include',
     });
 
     if (!response.ok) {
